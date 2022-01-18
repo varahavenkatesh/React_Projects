bd71e38ca344d167775af947ab436416369b197f
varahavenkatesh <varahavenkatesh@gmail.com>
2022-01-18 14:49:16 +0530

    New File MyComonent Committed

---
+++ redux/src/componenets/MyComponent.js
@@ -0,0 +1,54 @@
+import React,{useState,useEffect} from "react";
+
+
+const MyComponent =  () =>{
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+  
+    // Note: the empty deps array [] means
+    // this useEffect will run once
+    // similar to componentDidMount()
+    useEffect(() => {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(result);
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+    }, [])
+  
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+        {items.map(item => (
+          <li key={item.id}>
+             <b>Name: </b>{item.name} {item.username}
+             <b>  EMAIL :</b> {item.email} 
+            <br/>
+            <b>Address:</b> {item.address.street} {item.address.suite} 
+            {item.address.city} {item.address.zipcode} 
+            <br/>
+             <b>Web Company  Address:</b> {item.phone} {item.website}
+            {item.company.name} {item.company.catchPhrase}
+          </li>
+        ))}
+      </ul>
+      );
+    }
+  }
+
+  export default MyComponent;
